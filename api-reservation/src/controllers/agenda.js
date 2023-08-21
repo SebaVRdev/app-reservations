6198bdc5e59556977b54a217aa5abfa80c499395
SebaVRdev <sebarm177@gmail.com>
2023-08-20 21:01:31 -0400

    feat: :art: ADD Se agrega nuevo desarrollo que ayuda a la obstencion de informacion de la agenda y agregar registros de forma perosnalizada

--- api-reservation/src/controllers/agenda.js
+++ api-reservation/src/controllers/agenda.js
@@ -1,4 +1,4 @@
-import { Op } from "sequelize";
+import { Op, Sequelize } from "sequelize";
 import moment from "moment";
 
 import Agenda from "../models/Agenda.js";
@@ -6,11 +6,12 @@ import Court from "../models/Court.js";
 
 export const getAgendas = async (req, res) => {
     try {
-      const agendas = await Agenda.findAll({
+      const agendas = await Agenda.findAll();
+      /* const agendas = await Agenda.findAll({
         where : {
           [Op.like]: `July%`
         }
-      });
+      }); */
       
       if( agendas.length === 0 ) {
         return res.status(401).json({message: 'No hay registros guardados en Base de Datos'});
@@ -27,29 +28,48 @@ export const getAgendas = async (req, res) => {
     }
 };
 
+export const getAgendasInfo = async (req, res) => {
+  const agenda = await Agenda.findAll({
+    attributes: ['idAgenda', 'status', 'date'],
+    include: [
+      {
+        model: Court,
+        attributes: ['type'],
+        where: {
+          idCourt: Sequelize.col('Agenda.idCourt'),
+        },
+      },
+    ],
+  });
+
+  return res.status(200).json({
+    ok     : true,
+    message: "GET - SUCCESSFUL",
+    data   : agenda
+  });
+}
+
 export const getAgendasInDate = async (req, res) => {
     const { date, slug } = req.query;
     try {
-      const dateMoment = moment(date).format('MMMM Do YYYY, h:mm:ss a');
-      console.log(dateMoment)
-      const whereClause = {
-        //date: date,
-        [Op.like]: `2023-07-20%`
-      };
-  
-      // Verificar si el slug está presente en la consulta
-      if (slug) {
-        whereClause['$Court.slug$'] = slug;
-      }
-
+      const dateMoment = moment(date).format('YYYY-MM-DD');
+      console.log(dateMoment);
       const agendas = await Agenda.findAll({
-        attributes : ["idAgenda", "date","status"], 
-        where : whereClause,
+        attributes: ['idAgenda', 'status', 'date'],
         include: [
           {
-            model : Court
-          }
-        ]
+            model: Court,
+            attributes: ['type'],
+            where: {
+              idCourt: Sequelize.col('Agenda.idCourt'), // Ajusta la relación según tus modelos
+            },
+          },
+        ],
+        where: {
+          date: {
+            [Sequelize.Op.like]: `${dateMoment}%`,
+          },
+        },
       });
       
       if( agendas.length === 0 ) {
@@ -72,24 +92,28 @@ export const newAgenda = async (req, res) => {
     const { idCourt } = req.params;
     
     try {
-        const court = await Court.findByPk(idCourt);
-        if (!court) {
-            return res.status(401).json({message: 'Cancha no encontrada'});
-        }
+      /*La siguiente validacion se podria eliminar ya que existe un middleware 
+      que se va a ejecutar antes que este controlador, que va a verificar si esta disponible la 
+      cancha para respectiva fecha
+      */
+      const court = await Court.findByPk(idCourt);
+      if (!court) {
+          return res.status(401).json({message: 'Cancha no encontrada'});
+      }
 
-        const dateMoment = moment(date).format('MMMM Do YYYY, h:mm:ss a');;
-        console.log({dateMoment : dateMoment}); 
-
-        const agenda = await Agenda.create({
-            date : dateMoment
-        });
+      const dateMoment = moment(date).format('YYYY-MM-DD HH:mm:ss');
+      const agenda = await Agenda.create({
+          date : dateMoment
+      });
         
         // Agregamos la cancha que se manda por parametro a la agenda recien creada con la fecha asignada
         await court.addAgenda(agenda);
 
         return res.status(201).json({ 
-          message : "Disponibilidad creada con exito", 
-          dateMoment : dateMoment});
+            message : "Disponibilidad creada con exito", 
+            dateMoment : dateMoment
+          }
+        );
     } catch (error) {
       return res.status(500).json({ message : "Error inesperado en el servidor" })
     }
--- api-reservation/src/routes/agenda.routes.js
+++ api-reservation/src/routes/agenda.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { getAgendas, getAgendasInDate, newAgenda } from "../controllers/agenda.js";
+import { getAgendas, getAgendasInDate, getAgendasInfo, newAgenda } from "../controllers/agenda.js";
+import { verifyDisponibility } from "../middlewares/verifyDisponibility.js";
 
 const router = Router();
 
 router.get("/", getAgendas);
+router.get("/info", getAgendasInfo);
 router.get("/query/", getAgendasInDate);
-router.post("/:idCourt", newAgenda);
+router.post("/:idCourt", [verifyDisponibility] ,newAgenda);
 
 export default router;
